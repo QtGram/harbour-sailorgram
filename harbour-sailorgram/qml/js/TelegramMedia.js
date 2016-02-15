496af58216bf0b9997bb4a99443a871106e187ba
Dax <trogu.davide@gmail.com>
2016-02-15 17:41:33 +0100

    Added fallback icon for Venue and WebPage type messages

--- harbour-sailorgram/qml/js/TelegramMedia.js
+++ harbour-sailorgram/qml/js/TelegramMedia.js
@@ -19,8 +19,11 @@ function defaultThumbnail(message)
     if(mediatype === TelegramConstants.typeMessageMediaVideo)
         return "image://theme/icon-m-video";
 
-    if(mediatype === TelegramConstants.typeMessageMediaGeo)
+    if((mediatype === TelegramConstants.typeMessageMediaGeo) || (mediatype === TelegramConstants.typeMessageMediaVenue))
         return "image://theme/icon-m-location";
+
+    if(mediatype === TelegramConstants.typeMessageMediaWebPage)
+        return "image://theme/icon-m-link";
 
     if(mediatype === TelegramConstants.typeMessageMediaDocument)
     {
